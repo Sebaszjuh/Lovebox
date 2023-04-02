4b329fcacefe891da41d4e46ed547662ebac500b
jcvdg <jcgriendt@gmail.com>
2023-04-02 12:09:13 -0700

    Update styling

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import SockJsClient from 'react-stomp';
+import './css/index.css';
 
 
 const SOCKET_URL = 'http://localhost:9001/ws-message';
 
 const App = () => {
-    const [message, setMessage] = useState('Placeholder');
+    const [message, setMessage] = useState('Connected!');
 
     let onConnected = () => {
         console.log("Connected!!")
@@ -15,21 +16,21 @@ const App = () => {
     let onMessageReceived = (msg) => {
         console.log(msg)
         console.log(message);
-        setMessage(message + " \n " + msg)
+        setMessage(msg)
     }
 
     return (
-        <div>
+        <>
             <SockJsClient
                 url={SOCKET_URL}
                 topics={['/topic/message']}
                 onConnect={onConnected}
-                onDisconnect={console.log("Disconnected!")}
+                onDisconnect={() => console.log("Disconnected!")}
                 onMessage={msg => onMessageReceived(msg)}
                 debug={false}
             />
-            <div>{message}</div>
-        </div>
+            <div className="message">{message}</div>
+        </>
     );
 }
 
